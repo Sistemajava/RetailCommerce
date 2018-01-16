44fb4356b69af0a1a2e99f81cb4d7a317d9ca146
Peter <Peter@NB-SAMSUNG>
2018-01-16 15:59:10 -0300

    Merge origin/master
    Conflicts:
    	RetailPOS/src/retailpos/RetailPOS.java

--- RetailPOS/src/retailpos/frmSplash.java
+++ RetailPOS/src/retailpos/frmSplash.java
@@ -20,7 +20,7 @@ import javax.swing.Timer;
  * @author Peter
  */
 public class frmSplash extends javax.swing.JFrame {
-    public final static int ONE_SECOND = 27;
+    public final static int ONE_SECOND = 40;
     private final Timer timer;
     int cont;
 
@@ -42,8 +42,17 @@ public class frmSplash extends javax.swing.JFrame {
             cont++;
             //jLabel1.setText("nume es :"+cont);
             jProgressBar1.setValue(cont);
+            if(cont == 15){ETIQUETA.setText("REVOLUTION SOFTWARE 2018");}
+            if(cont == 30){ETIQUETA.setText("Cargando Librerías del Sistema");}
+            if(cont == 50){ETIQUETA.setText("Validando Espacio de Memoria Disponible");}
+            if(cont == 70){ETIQUETA.setText("Validando Conexión Internet");}
+            if(cont == 90){ETIQUETA.setText("Validando Conexión Base de Datos");}
+            if(cont == 108){ETIQUETA.setText("Ejecutando Aplicación");}   
+            
+            
+            
                         
-            if(cont==130){
+            if(cont==120){
                   timer.stop();
                   esconder();
                   
@@ -91,6 +100,7 @@ public class frmSplash extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jProgressBar1 = new javax.swing.JProgressBar();
+        ETIQUETA = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 0, 0));
@@ -101,6 +111,9 @@ public class frmSplash extends javax.swing.JFrame {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/89712.gif"))); // NOI18N
 
+        ETIQUETA.setFont(new java.awt.Font("Calibri", 0, 13)); // NOI18N
+        ETIQUETA.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -108,16 +121,23 @@ public class frmSplash extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 686, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 595, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 595, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(ETIQUETA, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(8, 8, 8)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(ETIQUETA, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -130,7 +150,7 @@ public class frmSplash extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -171,6 +191,7 @@ public class frmSplash extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel ETIQUETA;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JProgressBar jProgressBar1;
